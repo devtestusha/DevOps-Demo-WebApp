bbc57507f9954b373a9957f748b8888d4b65f760
devtestusha <70309653+devtestusha@users.noreply.github.com>
2020-09-11 07:33:29 +0530

    createPage commit 3

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://3.18.109.132:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://34.123.215.77:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
