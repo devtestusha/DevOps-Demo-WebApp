948904a3e795ec1349e071bb93bcea69efbcef64
devtestusha <70309653+devtestusha@users.noreply.github.com>
2020-09-14 12:20:50 +0530

    updated cancelPage after new ip update

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://34.123.215.77:5432/postgres";
+					String jdbcUrl="jdbc:postgresql://35.232.147.94:5432/postgres";
 					String username="postgres";
 					String password="password";
 					
